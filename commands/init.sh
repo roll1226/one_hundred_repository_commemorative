0d3f7064b07d0325a3a6bcc26479184c47f59090
roll1226 <tomo_yasu_4724@icloud.com>
2024-09-27 07:15:30 +0900

    fix[all]: command

--- commands/init.sh
+++ commands/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sh ./commands/sh/move_repository_root.sh
-sh ./commands/sh/frontend.sh Emulator
+sh ./commands/sh/admin.sh Emulator
+sh ./commands/sh/user.sh Emulator
 sh ./commands/sh/backend.sh Emulator
 sh ./commands/sh/functions.sh Emulator
 sh ./commands/sh/hasura.sh Emulator
