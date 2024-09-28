074d0d15466474b96db63ebe715871d2ef843f5e
roll1226 <tomo_yasu_4724@icloud.com>
2024-09-29 08:01:23 +0900

    feat[admin] vite.config.ts

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,11 +1,30 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3001,
-    host: "0.0.0.0",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    include: ["**/__tests__/**/*.test.(tsx|ts)"],
+    exclude: [
+      ...configDefaults.exclude,
+      "**/assets/**",
+      "**/coverage/**",
+      "**/dist/**",
+      "src/types/**", // TODO: 追々 Feature-Slices Design に合わせて修正
+    ],
+  },
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx", // TODO: 追々 Feature-Slices Design に合わせて修正
+    },
   },
 });
